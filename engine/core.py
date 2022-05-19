0f02c2d201e8a8d862884586007b1effeb2d3a37
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2022-05-19 11:26:08 -0300

    test driver

--- engine/core.py
+++ engine/core.py
@@ -8,10 +8,14 @@ class EngineModel(object):
     def __init__(self):
         options = Options()
         options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--incognito')
-        self.browser = webdriver.Chrome(
-            executable_path=ChromeDriverManager().install(), options=options)
+        # options.add_argument('--disable-gpu')
+        # options.add_argument('--incognito')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        self.browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         self.url = ''
         self.FILENAME = ''
 
